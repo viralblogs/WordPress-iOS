22e64bccc650332ecc64a6d4200d4be904086396
Leandro Alonso <contato@leandroalonso.com>
2021-04-12 16:44:52 -0300

    Create a AuthenticationHandler

--- WordPress/Classes/Utility/App Configuration/AppConfiguration.swift
+++ WordPress/Classes/Utility/App Configuration/AppConfiguration.swift
@@ -6,6 +6,5 @@ import Foundation
 @objc class AppConfiguration: NSObject {
     @objc static let isJetpack: Bool = false
     @objc static let allowSiteCreation: Bool = true
-    @objc static let jetpackLogin: Bool = false
     @objc static let allowSignUp: Bool = true
 }
