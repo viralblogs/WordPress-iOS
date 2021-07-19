d2c7b8f66b8c434fc2a9677e1e00baef42693969
thehenrybyrd <15107387+thehenrybyrd@users.noreply.github.com>
2021-07-19 11:14:34 +0300

    StaticTexts reference for Unified login link button

--- WordPress/WordPressUITests/Screens/Login/Unified/PasswordScreen.swift
+++ WordPress/WordPressUITests/Screens/Login/Unified/PasswordScreen.swift
@@ -4,7 +4,7 @@ import XCTest
 private struct ElementStringIDs {
     static let navBar = "WordPress.PasswordView"
     static let passwordTextField = "Password"
-    static let linkButton = "Send Link Button"
+    static let linkButton = "Get a login link by email"
     static let continueButton = "Continue Button"
     static let errorLabel = "Password Error"
 }
@@ -19,7 +19,7 @@ class PasswordScreen: BaseScreen {
         let app = XCUIApplication()
         navBar = app.navigationBars[ElementStringIDs.navBar]
         passwordTextField = app.secureTextFields[ElementStringIDs.passwordTextField]
-        linkButton = XCUIApplication().buttons[ElementStringIDs.linkButton]
+        linkButton = XCUIApplication().staticTexts[ElementStringIDs.linkButton]
         continueButton = app.buttons[ElementStringIDs.continueButton]
 
         super.init(element: passwordTextField)
